58beb2a17072e4e15c5a45eb9ed107dfa66a44ca
Xeomus <a180313@une.edu.mx>
2026-08-16 00:30:53 -0600

    feat: create initial Pacman game window

--- Pacman/src/main/java/pacman/game/GamePanel.java
+++ Pacman/src/main/java/pacman/game/GamePanel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class GamePanel extends JPanel {
     private static final int ROWS = 21;
     private static final int COLS = 19;
-    private static int TILE_SIZE= 32;
+    private static final int TILE_SIZE= 32;
 
     private static final int BOARD_WIDTH = COLS * TILE_SIZE;
     private static final int BOARD_HEIGHT = ROWS * TILE_SIZE;
