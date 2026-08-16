2891b4572f88a3e6172460d571610a75bfa39cfe
Xeomus <a180313@une.edu.mx>
2026-08-16 01:41:51 -0600

    feat: implement directional Pacman movemetn

--- Pacman/src/main/java/pacman/model/Pacman.java
+++ Pacman/src/main/java/pacman/model/Pacman.java
@@ -7,15 +7,48 @@ package pacman.model;
 * */
 public class Pacman extends Entity{
 
+    private Direction direction;
     /*
     * Call to parent constructor (super)
     * */
     public Pacman(Position position){
         super(position);
+        this.direction = Direction.RIGHT;
+    }
+
+    /*
+    * Pacman HAS-A Direction
+    * */
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction){
+        this.direction = direction;
     }
 
     @Override
     public void move() {
 
+        Position currentPosition = getPosition();
+
+        int x = currentPosition.getX();
+        int y = currentPosition.getY();
+
+        switch (direction){
+            case UP:
+                y++;
+                break;
+            case DOWN:
+                y--;
+                break;
+            case LEFT:
+                x--;
+                break;
+            case RIGHT:
+                x++;
+                break;
+        }
+        setPosition(new Position(x,y));
     }
 }
