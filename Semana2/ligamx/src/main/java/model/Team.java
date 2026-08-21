4089a8e5f500fe124a3cee1edfd022fe5bc8ba1f
Xeomus <a180313@une.edu.mx>
2026-08-21 12:05:10 -0600

    feat: add team

---
+++ Semana2/ligamx/src/main/java/model/Team.java
@@ -0,0 +1,68 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String city;
+    private String stadium;
+    private Integer foundingYear;
+
+    public Team() {}
+
+    public Team(String name, String city, String stadium, Integer foundingYear) {
+        this.name = name;
+        this.city = city;
+        this.stadium = stadium;
+        this.foundingYear = foundingYear;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStadium() {
+        return stadium;
+    }
+
+    public void setStadium(String stadium) {
+        this.stadium = stadium;
+    }
+
+    public Integer getFoundingYear() {
+        return foundingYear;
+    }
+
+    public void setFoundingYear(Integer foundingYear) {
+        this.foundingYear = foundingYear;
+    }
+}
