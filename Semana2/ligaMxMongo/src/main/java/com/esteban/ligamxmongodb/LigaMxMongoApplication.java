da7205c5902d4fcd486ce63a3329f8bddfc1223d
Xeomus <a180313@une.edu.mx>
2026-08-24 13:54:14 -0600

    Add files via upload

---
+++ Semana2/ligaMxMongo/src/main/java/com/esteban/ligamxmongodb/LigaMxMongoApplication.java
@@ -0,0 +1,12 @@
+package com.esteban.ligamxmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaMxMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaMxMongoApplication.class, args);
+	}
+}
