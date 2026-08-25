d69329ca2225a212a96166788d509f4f5bbdeed9
Xeomus <a180313@une.edu.mx>
2026-08-25 02:44:02 -0600

    Add Liga MX MongoDB API

---
+++ Semana2/ligaMxMongo/src/test/java/com/esteban/ligaMxMongo/LigaMxMongoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.esteban.ligamxmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaMxMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
